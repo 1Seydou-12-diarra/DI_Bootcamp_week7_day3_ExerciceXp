1240715befd580474e348373da5ee622de4acbdd
SAID DIARRASSOUBA <seydou.diarrassouba@imako.digital>
2023-02-09 14:03:20 +0000

    update

--- Exercice3/index.php
+++ Exercice3/index.php
@@ -1,6 +1,19 @@
- <?php
- session_start();
-$_SESSION['nom'] = 'seydou';
- echo '<pre>';
- print_r($_SESSION);
-echo '</pre>';
+<?php
+    // Start a PHP session
+    session_start();
+    $_SESSION["firstName"] = "Diarrassouba";
+    $_SESSION["lastName"] = "Seydou";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1</title>
+</head>
+<body>
+    <h3>Hello  <?= $_SESSION["firstName"] ." ". $_SESSION["lastName"] ?> </h3>
+</body>
+</html>
