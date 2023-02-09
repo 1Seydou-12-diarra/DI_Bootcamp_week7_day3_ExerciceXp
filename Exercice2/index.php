00f3a5d14552330e9d768237417cb4c4d847ead8
SAID DIARRASSOUBA <seydou.diarrassouba@imako.digital>
2023-02-09 14:17:26 +0000

    update

--- Exercice2/index.php
+++ Exercice2/index.php
@@ -1,37 +1,40 @@
 <?php
-    // Store Login Detail
-    setcookie("username", "Diarrassouba", time() + (86400 * 30), "/"); // 86400 = 1 day
+// Store Login Detail
+setcookie("username", "Diarrassouba", time() + (86400 * 30), "/"); // 86400 = 1 day
 
-    $submit = false;
-    $logIn = false;
-    // submit form
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $submit = true;
-        if (($_COOKIE["username"] == $_POST["username"])) {
-            $logIn = true;
-        }
-    }
+$submit = false;
+$logIn = false;
+// submit form
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $submit = true;
+  if (($_COOKIE["username"] == $_POST["username"])) {
+    $logIn = true;
+  }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercise 1</title>
-</head>
-<body>
-    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <label for="name">Username</label>
-        <input type="text" name="username" id="username">
-        <input type="submit" value="Valider">
-    </form>
 
-    <!-- msg connect -->
-    <?php if($submit): ?>
-        <h3>Login <?= $logIn ? "success" : "error" ?> </h3>
-    <?php endif; ?>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercise 1</title>
+</head>
+
+<body>
+  <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <label for="name">Username</label>
+    <input type="text" name="username" id="username">
+    <input type="submit" value="Valider">
+  </form>
+
+  <!-- msg connect -->
+  <?php if ($submit) : ?>
+    <h3 style="color:red;">Login <?= $logIn ? "success" : "nom incorect" ?> </h3>
+  <?php endif; ?>
 
 </body>
+
 </html>
--- Exercice3/index.php
+++ Exercice3/index.php
@@ -1,19 +1,22 @@
 <?php
-    // Start a PHP session
-    session_start();
-    $_SESSION["firstName"] = "Diarrassouba";
-    $_SESSION["lastName"] = "Seydou";
+// Start a PHP session
+session_start();
+$_SESSION["firstName"] = "Diarrassouba";
+$_SESSION["lastName"] = "Seydou";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercise 1</title>
 </head>
+
 <body>
-    <h3>Hello  <?= $_SESSION["firstName"] ." ". $_SESSION["lastName"] ?> </h3>
+    <h3>Hello <?= $_SESSION["firstName"] . " " . $_SESSION["lastName"] ?> </h3>
 </body>
+
 </html>
